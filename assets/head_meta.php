695b904bfa0ede96a75fa0ce2ec9559d3cbe9ae2
PrimusLV <K.Drivnieks@inbox.lv>
2016-10-24 15:40:49 +0000

    New Files

---
+++ assets/head_meta.php
@@ -0,0 +1,6 @@
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<!--[if lte IE 8]><?php $this->includeJs("ie/html5shiv") ?><![endif]-->
+<?php $this->includeCss("main") ?>
+<!--[if lte IE 8]><?php $this->includeCss("ie8") ?><![endif]-->
+<?php $this->headIncludes() ?>
